cb60955ff754a9221b64dc6b28b2b8301e73122f
kirilgrigel-cmyk <yehorzvoznikov@gmail.com>
2025-12-02 13:03:23 +0200

    Add Character and IEquippable

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -1,5 +1,20 @@
 ﻿namespace RPG_Game
 {
+    public interface IEquippable
+    {
+        string Name { get; }
+        void Equip();
+        void Unequip();
+    }
+
+    public abstract class Character
+    {
+        private string _name;
+        private int _health;
+        private int _maxHealth;
+        private int _strength;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
