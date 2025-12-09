6f25a60fa56a4c05c53c5bc6f2dca04f58a897a9
kirilgrigel-cmyk <yehorzvoznikov@gmail.com>
2025-12-09 12:35:12 +0200

    Add Item class

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -7,6 +7,25 @@
         void Unequip();
     }
 
+    public class Player
+    {
+
+    }
+
+    public abstract class Item
+    {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+
+        protected Item(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public abstract void Use(Player player);
+    }
+
     internal class Program
     {
         static void Main(string[] args)
