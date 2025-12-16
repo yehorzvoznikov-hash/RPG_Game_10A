5dee86a9661fabddd6c94efc34aa87b1dc2986cf
kirilgrigel-cmyk <yehorzvoznikov@gmail.com>
2025-12-16 12:51:34 +0200

    Add wooden sword

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -1,31 +1,11 @@
 ﻿namespace RPG_Game
 {
-    public class Weapon : Item, IEquippable
+    public class WoodenSword : Weapon
     {
-        public int Damage {get; protected set;}
-
-        public Weapon(string name, string description, int damage) : base(name, description)
+        public WoodenSword() : base("Дерев'яний меч", "+5 до атаки", 5)
         {
-            Damage = damage;
-        }
-
-        public void Equip(Player player)
-        {
-            Console.WriteLine($"{Name} екіпіровано! (+{Damage} до атаки)");
-        }
-
-        public void Unequip(Player player)
-        {
-            Console.WriteLine($"{Name} знято.");
-        }
-
-        public override void Use(Player player)
-        {
-            Console.WriteLine($"{player.Name} екіпірує {Name}!");
-            Equip(player);
         }
     }
-
 
     internal class Program
     {
---
+++ RPG_Game/RPG_Game/Weapon.cs
@@ -0,0 +1,28 @@
+﻿namespace RPG_Game
+{
+    public class Weapon : Item, IEquippable
+    {
+        public int Damage {get; protected set;}
+
+        public Weapon(string name, string description, int damage) : base(name, description)
+        {
+            Damage = damage;
+        }
+
+        public void Equip(Player player)
+        {
+            Console.WriteLine($"{Name} екіпіровано! (+{Damage} до атаки)");
+        }
+
+        public void Unequip(Player player)
+        {
+            Console.WriteLine($"{Name} знято.");
+        }
+
+        public override void Use(Player player)
+        {
+            Console.WriteLine($"{player.Name} екіпірує {Name}!");
+            Equip(player);
+        }
+    }
+}
