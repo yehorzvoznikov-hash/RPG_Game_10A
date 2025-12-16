0b9e450a476f204b2cfba9fca8daf2aa72b1846a
kirilgrigel-cmyk <yehorzvoznikov@gmail.com>
2025-12-16 13:05:07 +0200

    Add Goblin

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -19,7 +19,22 @@
         public override void Use(Player player)
         {
             Console.WriteLine($"{player.Name} використовує {Name}");
-            player.Heal();
+            player.Heal(_healAmount);
+        }
+    }
+
+    public class Goblin : Enemy
+    {
+        public Goblin() : base("Гоблін", 40, 8, 30)
+        {
+            AddLoot(new HealthPotion());
+        }
+
+        public override void Attack(Character target)
+        {
+            var damage = Strength;
+            Console.WriteLine($"{Name} б'є кинджалом і завдає {damage}");
+            target.TakeDamage(damage);
         }
     }
 
